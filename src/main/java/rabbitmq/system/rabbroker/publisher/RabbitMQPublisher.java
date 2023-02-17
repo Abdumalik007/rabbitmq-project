8413dd33689e5f197bf6eeb861d3e98e28f22378
Abdumalik <abdumaliksobirov007@gmail.com>
2023-02-17 10:30:44 +0500

    Publisher added

---
+++ src/main/java/rabbitmq/system/rabbroker/publisher/RabbitMQPublisher.java
@@ -0,0 +1,34 @@
+package rabbitmq.system.rabbroker.publisher;
+
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQPublisher {
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(RabbitMQPublisher.class);
+
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent -> %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+
+
+}
+
+
